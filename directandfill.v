dcef45411ef4d570cf3f1029a9024cc021706d06
amazingemw <amazingemw@gmail.com>
2017-06-01 20:07:50 +0530

    injector compiled
    Understood task integration problem and modified the directandfill logic

--- directandfill.v
+++ directandfill.v
@@ -1,41 +1,45 @@
 module directandfill();
-reg [2:0] col;
-reg [2:0] row;
 
-task direction;
+task directandfill;
 	input [5:0] addr;
 	input [5:0] localad;
 	output [5:0] adr;
-	output [4:0] dir;
-	output flag;
+	output [2:0] dir;
 
-	begin
-	row = localad[5:3];  //Destination x coordinate
-	col = localad[2:0];  //Destination y coordinate
-	if (addr === 6'bz) begin
-		adr = localad;	
-		if ( col > 3'b100) begin
-			dir = 5'b00001;             	 //EAST
-        	end
-        	if ( col < 3'b100) begin
-			dir = 5'b00010;             	 //WEST
-        	end 
+	reg [2:0] col;
+	reg [2:0] row;
+
+	begin 
+	
+        	if (addr === 6'bz) begin
+			adr = localad;
+	 		row = localad[5:3];  
+			col = localad[2:0]; 	
+		end else begin
+			adr = addr;
+			row = addr[5:3];  
+			col = addr[2:0]; 
+		end
+	
+        	if ( col > 3'b100) begin
+			dir = 3'b000;             	//EAST
+       		end
+       		if ( col < 3'b100) begin
+			dir = 5'b001;             	//WEST
+       		end 
 		if ( col === 3'b100) begin
 			if ( row > 3'b100) begin
-				dir = 5'b00100;             //NORTH
-        		end
+				dir = 5'b010;           //NORTH
+       			end
 			if ( row < 3'b100) begin
-				dir = 5'b01000;             //SOUTH
-        		end
+				dir = 5'b011;           //SOUTH
+      			end
 			if ( row === 3'b100) begin
-				dir = 5'b10000; 	    //LOCAL    
-				flag = 1;        
-        		end
-        	end
-	end else begin
-		adr = addr;
-	end
-	end
+				dir = 5'b100; 	        //LOCAL    
+			end
+       		end
+	
+	end  //begin's end
 endtask
 
 endmodule
